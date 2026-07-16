38cdd2aa7a8333f5ec0fc1e9275118095b68f522
TiRADfolk <Tiradfolk@gmail.com>
2026-07-16 20:01:08 +0200

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        creme: '#FAF6EE',
+        foret: '#1E352F',
+        terracotta: '#D36B4F',
+        bois: '#3D251E',
+      }
+    }
+  }
+}
